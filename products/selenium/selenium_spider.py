2cba08e9cea065a7aa9d67fd9c0cf823719368d5
Fiete Ostkamp <F.Ostkamp@web.de>
2022-04-09 11:01:40 +0200

    clean up code

--- products/selenium/selenium_spider.py
+++ products/selenium/selenium_spider.py
@@ -1,11 +1,9 @@
 import hashlib
 import random
 import time
-import os
 import pandas as pd
 from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.service import Service
@@ -13,8 +11,6 @@ from selenium.webdriver.firefox.service import Service
 # https://www.selenium.dev/documentation/
 options = Options()
 profile = FirefoxProfile()
-# profile.add_extension('uBlock0_1.38.6.firefox.xpi')
-# profile.set_preference("browser.privatebrowsing.autostart", True)
 options.profile = profile
 options.headless = True
 service = Service(executable_path='/usr/bin/geckodriver')
@@ -31,7 +27,7 @@ for url in urls:
     driver.get(url)
     url_hash = hashlib.md5(url.encode("utf8")).hexdigest()
     outpath = project_path.parent.joinpath(f'out/screenshots/{url_hash}.png')
-    # wait between 0 and 1 seconds to not DoS the websites
+    # wait between 2 and 3 seconds to not DoS the websites
     time.sleep(random.uniform(2,3))
     print(outpath)
     driver.save_full_page_screenshot(outpath.as_posix())
